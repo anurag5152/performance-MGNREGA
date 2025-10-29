c057f1c44e317709076e17430b608145934e43e7
anurag5152 <anurag.5152.kumar@gmail.com>
2025-10-29 09:57:45 +0530

    updated

--- server.js
+++ server.js
@@ -186,10 +186,11 @@ app.get("/api/mgnrega", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
+
 
 app.listen(PORT, async () => {
   try {
